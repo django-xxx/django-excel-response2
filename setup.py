5bae88b6397367266d7d713e070b4e652481dfed
Brightcells <brightcells@gmail.com>
2015-09-22 15:38:29 +0800

    upload PyPI

--- setup.py
+++ setup.py
@@ -1,31 +1,33 @@
 # -*- coding: utf-8 -*-
-
 from __future__ import with_statement
 from setuptools import setup
 
 
-version = '2.0.1'
+version = '1.0.1'
 
 
 setup(
-    name='CodeConvert',
+    name='django-excel-response2',
     version=version,
-    keywords='CodeConvert unicode utf8 utf-8 gbk latin1 raw_unicode_escape',
-    description="Code Convert for Humans",
-    long_description='CodeConvert is a simple code convert script(library) for Python, built for human beings.',
+    keywords='django-excel-response django-excel-response2',
+    description="A function extends of Tarken's django-excel-response",
+    long_description='',
 
-    url='https://github.com/Brightcells/CodeConvert',
+    url='https://github.com/Brightcells/django-excel-response2',
 
     author='Hackathon',
     author_email='kimi.huang@brightcells.com',
 
-    py_modules=['CodeConvert'],
-    install_requires=[],
+    py_modules=['excel_response2'],
+    install_requires = ['xlwt', 'pytz'],
 
     classifiers=[
         'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
         'Intended Audience :: Developers',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Office/Business :: Financial :: Spreadsheet',
     ],
 )
