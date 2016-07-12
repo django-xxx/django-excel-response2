5f2417acaef7222565cfbc38ffffe8f3d0813e0f
Brightcells <brightcells@gmail.com>
2016-07-12 14:12:25 +0800

    add param blanks_for_none and auto_adjust_width inspired by danpetrikin

--- excel_response2.py
+++ excel_response2.py
@@ -1,23 +1,9 @@
 # -*- coding:utf-8 -*-
 
-"""
-A function extends of Tarken's django-excel-response
-
-django-excel-response
-1、djangosnippets - http://djangosnippets.org/snippets/1151/
-2、pypi - https://pypi.python.org/pypi/django-excel-response/1.0
-
-When using Tarken's django-excel-response. We find that
-Chinese is messed code when we open .xls in Mac OS.
-As discussed in http://segmentfault.com/q/1010000000095546.
-We realize django-excel-response2
-Based on Tarken's django-excel-response to solve this problem
-By adding a Param named font to set font.
-"""
-
 import datetime
 
 import pytz
+import xlwt
 
 from django import http
 from django.conf import settings
@@ -42,7 +28,140 @@ except ImportError:
         from io import StringIO
 
 
-def __init__(self, data, output_name='excel_data', headers=None, force_csv=False, encoding='utf8', font=''):
+# Min (Max. Rows) for Widely Used Excel
+# http://superuser.com/questions/366468/what-is-the-maximum-allowed-rows-in-a-microsoft-excel-xls-or-xlsx
+EXCEL_MAXIMUM_ALLOWED_ROWS = 65536
+# Column Width Limit For ``xlwt``
+# https://github.com/python-excel/xlwt/blob/master/xlwt/Column.py#L22
+EXCEL_MAXIMUM_ALLOWED_COLUMN_WIDTH = 65535
+
+# https://github.com/urwid/urwid/blob/master/urwid/old_str_util.py
+WIDTHS = [
+    (126, 1),
+    (159, 0),
+    (687, 1),
+    (710, 0),
+    (711, 1),
+    (727, 0),
+    (733, 1),
+    (879, 0),
+    (1154, 1),
+    (1161, 0),
+    (4347, 1),
+    (4447, 2),
+    (7467, 1),
+    (7521, 0),
+    (8369, 1),
+    (8426, 0),
+    (9000, 1),
+    (9002, 2),
+    (11021, 1),
+    (12350, 2),
+    (12351, 1),
+    (12438, 2),
+    (12442, 0),
+    (19893, 2),
+    (19967, 1),
+    (55203, 2),
+    (63743, 1),
+    (64106, 2),
+    (65039, 1),
+    (65059, 0),
+    (65131, 2),
+    (65279, 1),
+    (65376, 2),
+    (65500, 1),
+    (65510, 2),
+    (120831, 1),
+    (262141, 2),
+    (1114109, 1),
+]
+
+
+def get_width(o):
+    """ Return the screen column width for unicode ordinal o. """
+    if o == 0xe or o == 0xf:
+        return 0
+    for num, wid in WIDTHS:
+        if o <= num:
+            return wid
+    return 1
+
+
+def calc_width(s):
+    """ Should Improved """
+    return sum([get_width(ord(i)) for i in s])
+
+
+@property
+def as_xls(self):
+    book = xlwt.Workbook(encoding=self.encoding)
+    sheet = book.add_sheet(self.sheet_name)
+
+    styles = {
+        'datetime': xlwt.easyxf(num_format_str='yyyy-mm-dd hh:mm:ss'),
+        'date': xlwt.easyxf(num_format_str='yyyy-mm-dd'),
+        'time': xlwt.easyxf(num_format_str='hh:mm:ss'),
+        'font': xlwt.easyxf('%s %s' % ('font:', self.font)),
+        'default': xlwt.Style.default_style,
+    }
+
+    widths = {}
+    for rowx, row in enumerate(self.data):
+        for colx, value in enumerate(row):
+            if value is None and self.blanks_for_none:
+                value = ''
+
+            if isinstance(value, datetime.datetime):
+                if timezone.is_aware(value):
+                    value = timezone.make_naive(value, pytz.timezone(settings.TIME_ZONE))
+                cell_style = styles['datetime']
+            elif isinstance(value, datetime.date):
+                cell_style = styles['date']
+            elif isinstance(value, datetime.time):
+                cell_style = styles['time']
+            elif self.font:
+                cell_style = styles['font']
+            else:
+                cell_style = styles['default']
+
+            sheet.write(rowx, colx, value, style=cell_style)
+
+            if self.auto_adjust_width:
+                width = calc_width(value) * 256 if isinstance(value, basestring) else calc_width(str(value)) * 256
+                if width > widths.get(colx, 0):
+                    width = min(width, self.EXCEL_MAXIMUM_ALLOWED_COLUMN_WIDTH)
+                    widths[colx] = width
+                    sheet.col(colx).width = width
+
+    book.save(self.output)
+
+
+@property
+def as_csv(self):
+    for row in self.data:
+        out_row = []
+        for value in row:
+            if value is None and self.blanks_for_none:
+                value = ''
+            if not isinstance(value, basestring):
+                value = unicode(value)
+            value = value.encode(self.encoding)
+            out_row.append(value.replace('"', '""'))
+        self.output.write('"%s"\n' % '","'.join(out_row))
+
+
+def __init__(self, data, output_name='excel_data', headers=None, force_csv=False, encoding='utf8', font='', sheet_name='Sheet 1', blanks_for_none=True, auto_adjust_width=True):
+
+    self.data = data
+    self.output_name = output_name
+    self.headers = headers
+    self.force_csv = force_csv
+    self.encoding = encoding
+    self.font = font
+    self.sheet_name = sheet_name
+    self.blanks_for_none = blanks_for_none
+    self.auto_adjust_width = auto_adjust_width
 
     # Make sure we've got the right type of data to work with
     # ``list index out of range`` if data is ``[]``
@@ -61,67 +180,30 @@ def __init__(self, data, output_name='excel_data', headers=None, force_csv=False
             valid_data = True
     assert valid_data is True, 'ExcelResponse requires a sequence of sequences'
 
-    output = StringIO()
+    self.output = StringIO()
     # Excel has a limit on number of rows; if we have more than that, make a csv
-    use_xls = False
-    if len(data) <= 65536 and not force_csv:
-        try:
-            import xlwt
-            use_xls = True
-        except ImportError:
-            # xlwt doesn't exist; fall back to csv
-            pass
-    if use_xls:
-        book = xlwt.Workbook(encoding=encoding)
-        sheet = book.add_sheet('Sheet 1')
-        styles = {
-            'datetime': xlwt.easyxf(num_format_str='yyyy-mm-dd hh:mm:ss'),
-            'date': xlwt.easyxf(num_format_str='yyyy-mm-dd'),
-            'time': xlwt.easyxf(num_format_str='hh:mm:ss'),
-            'font': xlwt.easyxf('%s %s' % ('font:', font)),
-            'default': xlwt.Style.default_style,
-        }
-
-        for rowx, row in enumerate(data):
-            for colx, value in enumerate(row):
-                if isinstance(value, datetime.datetime):
-                    if timezone.is_aware(value):
-                        value = timezone.make_naive(value, pytz.timezone(settings.TIME_ZONE))
-                    cell_style = styles['datetime']
-                elif isinstance(value, datetime.date):
-                    cell_style = styles['date']
-                elif isinstance(value, datetime.time):
-                    cell_style = styles['time']
-                elif font:
-                    cell_style = styles['font']
-                else:
-                    cell_style = styles['default']
-                sheet.write(rowx, colx, value, style=cell_style)
-        book.save(output)
-        content_type = 'application/vnd.ms-excel'
-        file_ext = 'xls'
-    else:
-        for row in data:
-            out_row = []
-            for value in row:
-                if not isinstance(value, basestring):
-                    value = unicode(value)
-                value = value.encode(encoding)
-                out_row.append(value.replace('"', '""'))
-            output.write('"%s"\n' % '","'.join(out_row))
-        content_type = 'text/csv'
-        file_ext = 'csv'
-    output.seek(0)
-    super(ExcelResponse, self).__init__(output, content_type=content_type)
-    self['Content-Disposition'] = 'attachment;filename="%s.%s"' % (output_name.replace('"', '\"'), file_ext)
+    use_xls = True if len(self.data) <= self.EXCEL_MAXIMUM_ALLOWED_ROWS and not self.force_csv else False
+    _, content_type, file_ext = (self.as_xls, 'application/vnd.ms-excel', 'xls') if use_xls else (self.as_csv, 'text/csv', 'csv')
+    self.output.seek(0)
+    super(ExcelResponse, self).__init__(self.output, content_type=content_type)
+    self['Content-Disposition'] = 'attachment;filename="%s.%s"' % (self.output_name.replace('"', '\"'), file_ext)
 
 
 names = dir(http)
 
 
+clsdict = {
+    'EXCEL_MAXIMUM_ALLOWED_ROWS': EXCEL_MAXIMUM_ALLOWED_ROWS,
+    'EXCEL_MAXIMUM_ALLOWED_COLUMN_WIDTH': EXCEL_MAXIMUM_ALLOWED_COLUMN_WIDTH,
+    '__init__': __init__,
+    'as_xls': as_xls,
+    'as_csv': as_csv,
+}
+
+
 if 'FileResponse' in names:
-    ExcelResponse = type('ExcelResponse', (http.FileResponse, ), dict(__init__=__init__))
+    ExcelResponse = type('ExcelResponse', (http.FileResponse, ), clsdict)
 elif 'StreamingHttpResponse' in names:
-    ExcelResponse = type('StreamingHttpResponse', (http.StreamingHttpResponse, ), dict(__init__=__init__))
+    ExcelResponse = type('StreamingHttpResponse', (http.StreamingHttpResponse, ), clsdict)
 else:
-    ExcelResponse = type('HttpResponse', (http.HttpResponse, ), dict(__init__=__init__))
+    ExcelResponse = type('HttpResponse', (http.HttpResponse, ), clsdict)
